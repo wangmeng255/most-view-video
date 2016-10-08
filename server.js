f959e998224a96f88f93e648286bbf57a65b8c8a
Meng Wang <wangmeng255@gmail.com>
2016-10-08 05:54:42 +0000

    remove heroku

--- server.js
+++ server.js
@@ -1,13 +1,5 @@
 var express = require('express');
 var app = new express();
-var fs = require('fs');
-var mustache = require('mustache'); // bring in mustache template engine
-
-app.get('/', function(req, res) {
-    var page = fs.readFileSync('index.mustache', "utf8");
-    var html = mustache.to_html(page, {production:process.env.NODE_ENV == 'production'});
-    res.end(html);
-});
 
 app.use(express.static('build'));
 
