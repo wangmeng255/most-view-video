395ff8412a88cd5e8f81c2dc9bb0c0b2c9537556
Meng Wang <wangmeng255@gmail.com>
2016-11-08 06:58:46 +0000

    change back browserHistory to hashHistory

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var Provider = require('react-redux').Provider;
 var router = require('react-router');
 var Router = router.Router;
 var Route = router.Route;
-var browserHistory = router.browserHistory;
+var hashHistory = router.hashHistory;
 
 var store = require('./store');
 var search = require('./components/search');
@@ -13,7 +13,7 @@ var Container = search.Container;
 document.addEventListener('DOMContentLoaded', function() {
     ReactDOM.render(
         <Provider store={store}>
-            <Router history={browserHistory}>
+            <Router history={hashHistory}>
                 <Route path="/" component={Container}>
                     <Route path="/?search/q=:keyword&span=:after-:before" onChange={search.Search} />
                     <Route path="/?search/q=:keyword&span=:after-:before" onEnter={search.filter} />
